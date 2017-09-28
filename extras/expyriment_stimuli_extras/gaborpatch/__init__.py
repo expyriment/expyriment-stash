c0cbbe356c44fe925c64fdbed759de0741c8166a
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2017-09-28 17:38:55 +0200

    GaborPatch bug

--- extras/expyriment_stimuli_extras/gaborpatch/__init__.py
+++ extras/expyriment_stimuli_extras/gaborpatch/__init__.py
@@ -19,36 +19,43 @@ __date__ = ''
 class GaborPatch:
     """A class implementing a Gabor Patch."""
 
-    def __init__(self, size=300, position=None, lambda_=10, theta=15,
-                sigma=20, phase=.25, trim=.005):
+    def __init__(self, position=None,
+                 sigma=55,
+                 theta=35,
+                 lambda_=12.5,
+                 phase=0.5,
+                 psi=120,
+                 gamma=1,
+                 background_colour=constants.C_DARKGREY):
         """Create a Gabor Patch.
-
         Parameters
         ----------
-        size : (int, int), optional
-            size (x, y) of the mask (default=300)
         position  : (int, int), optional
             position of the mask stimulus
-        lambda_ : int, optional
-            Spatial frequency (pixel per cycle) (default=10)
-        theta : int or float, optional
-            Grating orientation in degrees (default=15)
         sigma : int or float, optional
             gaussian standard deviation (in pixels) (default=20)
+        theta : int or float, optional
+            Grating orientation in degrees (default=35)
+        lambda_ : int, optional
+            Spatial frequency (pixel per cycle) (default=10)
         phase : float
-            0 to 1 inclusive (default=.25)
-        trim : float
-            trim off Gaussian values smaller than this (default=.005)
-
+            0 to 1 inclusive (default=.5)
+        psi : int, optional
+            0 to 1 inclusive (default=1)
+        gamma : float
+            0 to 1 inclusive (default=1)
+        background_colour : (int,int,int), optional
+            colour of the background, default: misc.constants.C_DARKGREY
         Notes
         -----
         The background colour of the stimulus depends of the parameters of
         the Gabor patch and can be determined (e.g. for plotting) with the
         property `GaborPatch.background_colour`.
-
         """
 
         from ._gaborpath import GaborPath
         self.__class__ = GaborPatch
-        GaborPatch.__init__(self, size, position, lambda_, theta, sigma,
-                            phase, trim)
+        GaborPatch.__init__(self, position=position,
+                            sigma=sigma, theta=theta, lambda_=lambda_, 
+                            phase=phase, psi=psi, gamma=gamma, 
+                            background_colour=background_colour)
